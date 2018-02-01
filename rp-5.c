b228e81f65eda6b1a9e38d9555c7e333d0ad919a
rathinap6 <35958265+rathinap6@users.noreply.github.com>
2018-02-01 16:24:59 +0530

    Create rp-5.c

---
+++ rp-5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter the year");
+    scanf("%d",&a);
+    if(a%4==0)
+    printf("Leap year");
+    else
+    printf("Not leap year");
+}
+   
