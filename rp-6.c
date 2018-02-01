99f62be9df044b4d2c0ce6206db488fe80576825
rathinap6 <35958265+rathinap6@users.noreply.github.com>
2018-02-01 16:34:51 +0530

    Create rp-6.c

---
+++ rp-6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+    int i,n;
+    printf("Enter the number of times");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+    printf("Hello\n");
+
+    }
+}
+
+
