db63942c3b1a320e87f04401e62246aa9ea0e552
rathinap6 <35958265+rathinap6@users.noreply.github.com>
2018-02-01 16:57:35 +0530

    Update rp-10.c

--- rp-10.c
+++ rp-10.c
@@ -2,12 +2,12 @@
 #include<math.h>
 void main()
 {
-  int a,n,c;
-  printf("enter value for a:");
-  scanf("%d",&a);
+  int i,n,c;
+  printf("enter value for i:");
+  scanf("%d",&i);
    printf("enter value for power:");
    scanf("%d",&n);
-   c=pow(a,n);
+   c=pow(i,n);
   printf("the number of digits in integer:%d",c);
 }
   
